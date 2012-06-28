017e5c94daf2d867550b1d4ad94d3a7f8ec310d2
Anthony Ortiz <ortiz.anthony@gmail.com>
2012-06-28 08:03:39 -0400

    Adding koans to git repo.

--- ex41.rb
+++ ex41.rb
@@ -91,6 +91,93 @@ def central_corridor()
             return :death
         end
     end
+    
+    def the_bridge()
+      puts "You burst onto the Bridge with the neutron destruct bomb"
+      puts "under your arm and surprise 5 Gothons who are trying to"
+      puts "take control of the ship.  Each of them has an even uglier"
+      puts "clown costume than the last.  They haven't pulled their"
+      puts "weapons out yet, as they see the active bomb under your"
+      puts "arm and don't want to set it off."
+
+      prompt()
+      action = gets.chomp()
+
+      if action == "throw the bomb"
+        puts "In a panic you throw the bomb at the group of Gothons"
+        puts "and make a leap for the door.  Right as you drop it a"
+        puts "Gothon shoots you right in the back killing you."
+        puts "As you die you see another Gothon frantically try to disarm"
+        puts "the bomb. You die knowing they will probably blow up when"
+        puts "it goes off."
+        return :death
+
+      elsif action == "slowly place the bomb"
+        puts "You point your blaster at the bomb under your arm"
+        puts "and the Gothons put their hands up and start to sweat."
+        puts "You inch backward to the door, open it, and then carefully"
+        puts "place the bomb on the floor, pointing your blaster at it."
+        puts "You then jump back through the door, punch the close button"
+        puts "and blast the lock so the Gothons can't get out."
+        puts "Now that the bomb is placed you run to the escape pod to"
+        puts "get off this tin can."
+        return :escape_pod
+      else
+        puts "DOES NOT COMPUTE!"
+        return :the_bridge
+      end
+    end
+    
+    def escape_pod()
+      puts "You rush through the ship desperately trying to make it to"
+      puts "the escape pod before the whole ship explodes.  It seems like"
+      puts "hardly any Gothons are on the ship, so your run is clear of"
+      puts "interference.  You get to the chamber with the escape pods, and"
+      puts "now need to pick one to take.  Some of them could be damaged"
+      puts "but you don't have time to look.  There's 5 pods, which one"
+      puts "do you take?"
+
+      good_pod = rand(5)+1 
+      print "[pod #]>"
+      guess = gets.chomp()
+
+      if guess.to_i != good_pod
+        puts "You jump into pod %s and hit the eject button." % guess
+        puts "The pod escapes out into the void of space, then"
+        puts "implodes as the hull ruptures, crushing your body"
+        puts "into jam jelly."
+        return :death
+      else
+        puts "You jump into pod %s and hit the eject button." % guess
+        puts "The pod easily slides out into space heading to"
+        puts "the planet below.  As it flies to the planet, you look"
+        puts "back and see your ship implode then explode like a"
+        puts "bright star, taking out the Gothon ship at the same"
+        puts "time.  You won!"
+        Process.exit(0)
+      end
+    end
+
+    ROOMS = {
+      :death => method(:death),
+      :central_corridor => method(:central_corridor),
+      :laser_weapon_armory => method(:laser_weapon_armory),
+      :the_bridge => method(:the_bridge),
+      :escape_pod => method(:escape_pod)
+    }
+
+    def runner(map, start)
+      next_one = start
+
+      while true
+        room = map[next_one]
+        puts "\n--------"
+        next_one = room.call()
+      end
+    end
+
+    runner(ROOMS, :central_corridor)
+
   
 end
 
